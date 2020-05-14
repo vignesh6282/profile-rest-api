57e7bfb64f2accd0f9067b28bde16dc990413949
vignesh6282 <vignesh11122@gmail.com>
2020-05-14 16:02:17 +0530

    Added HelloApiView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-...
 
 
 class UserProfileManager(BaseUserManager):
